b312ce9f4ab8a3a8d1e359b7c2e3af2e996999d6
Nikhil Jain <nikhil@gluu.life>
2020-11-24 19:51:09 +0530

    Started with module.exports with multiple values and class

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 const tutorial = require('./tutorial')
-console.log("Hello World from NodeJs ", tutorial(1, 3))
+console.log("Hello World from NodeJs ", tutorial.sum(1, 3))
+console.log("Hello World from NodeJs ", tutorial.PI)
+console.log("Hello World from NodeJs ", new tutorial.MyClass())
--- tutorial.js
+++ tutorial.js
@@ -1,4 +1,13 @@
 const sum = (num1, num2) => num1 + num2
+const PI = 3.14
 
-module.exports = sum;
+class MyClass {
+    constructor() {
+        console.log("I am awaken")
+    }
+}
+
+module.exports.sum = sum
+module.exports.PI = PI
+module.exports.MyClass = MyClass
 console.log(sum(1, 2))
