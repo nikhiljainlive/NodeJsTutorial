b102e03f40c088af4c6fcbd547af32ece2e6ca22
Nikhil Jain <nikhil@gluu.life>
2020-11-24 19:55:34 +0530

    Tried module.exports with nested values and class

--- tutorial.js
+++ tutorial.js
@@ -1,13 +1,13 @@
-const sum = (num1, num2) => num1 + num2
-const PI = 3.14
+const tutorial = {
+    sum: sum = (num1, num2) => num1 + num2,
+    PI: PI = 3.14,
 
-class MyClass {
-    constructor() {
-        console.log("I am awaken")
+    MyClass: class MyClass {
+        constructor() {
+            console.log("I am awaken")
+        }
     }
+
 }
 
-module.exports.sum = sum
-module.exports.PI = PI
-module.exports.MyClass = MyClass
-console.log(sum(1, 2))
+module.exports = tutorial
