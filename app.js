2af61afa53ec770fba026a900c6a2925fb6604fa
Nikhil Jain <nikhil@gluu.life>
2020-11-24 20:04:40 +0530

    Implemented Simple Event Emitter

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
-const tutorial = require('./tutorial')
-console.log("Hello World from NodeJs ", tutorial.sum(1, 3))
-console.log("Hello World from NodeJs ", tutorial.PI)
-console.log("Hello World from NodeJs ", new tutorial.MyClass())
+const EventEmitter = require('events')
+const { sum } = require('./tutorial')
+
+const eventEmitter = new EventEmitter()
+eventEmitter.on('tutorial', (num1, num2) => {
+    console.log('tutorial event has occurred', sum(num1, num2))
+})
+
+eventEmitter.emit('tutorial', 1, 2)
