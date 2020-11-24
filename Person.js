ec2d3d3a685171143703efb8f94914d7d7c403c3
Nikhil Jain <nikhil@gluu.life>
2020-11-24 20:16:14 +0530

    Implemented custom Event Emitter

---
+++ Person.js
@@ -0,0 +1,14 @@
+const EventEmitter = require('events')
+
+class Person extends EventEmitter {
+    constructor(name) {
+        super()
+        this._name = name
+    }
+
+    get name() {
+        return this._name
+    }
+}
+
+module.exports = Person
--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const EventEmitter = require('events')
 const { sum } = require('./tutorial')
+const Person = require('./Person')
 
 const eventEmitter = new EventEmitter()
 eventEmitter.on('tutorial', (num1, num2) => {
@@ -7,3 +8,14 @@ eventEmitter.on('tutorial', (num1, num2) => {
 })
 
 eventEmitter.emit('tutorial', 1, 2)
+
+let nick = new Person("Nick")
+let neo = new Person("Neo")
+neo.on('name', () => console.log("My name is ", neo.name))
+
+nick.on('name', () => {
+    console.log("My name is ", nick.name)
+})
+
+nick.emit('name')
+neo.emit('name')
