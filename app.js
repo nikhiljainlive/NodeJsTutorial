7c740d8eb8f2640e3344ab8a118ae5ce58e4e4cd
Nikhil Jain <nikhil@gluu.life>
2021-01-17 15:50:36 +0530

    Implemented pipe statements with streams and zlib module

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const fs = require('fs')
+const zlib = require('zlib')
 
-const fileName = "example.txt"
-const readStream = fs.createReadStream(fileName, "utf8")
+const readStream = fs.createReadStream("example.txt", "utf8")
 const writeStream = fs.createWriteStream("example1.txt")
 
-readStream.on("data", chunk => {
-    console.log(chunk);
-    writeStream.write(chunk)
-})
+// sourceStream | destinationStream
+// readStream.pipe(writeStream)
+
+const gZip = zlib.createGzip()
+const gZipWriteStream = fs.createWriteStream("example1.txt.gz")
+readStream.pipe(gZip).pipe(gZipWriteStream)
+
+
+setTimeout(() => {
+    const gZipReadStream = fs.createReadStream("./example1.txt.gz")
+    const gUnzip = zlib.createGunzip()
+    gUnzip.on('error', error => console.log(error))
+    const gUnzipWriteStream = fs.createWriteStream("uncompressed.txt")
+    gZipReadStream.pipe(gUnzip).pipe(gUnzipWriteStream)
+}, 2000);
