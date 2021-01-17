cc1410408d36c0f70df134f496551acbb783924f
Nikhil Jain <nikhil@gluu.life>
2021-01-17 15:57:43 +0530

    Implemented simple http request using http module

--- app.js
+++ app.js
@@ -1,21 +1,10 @@
-const fs = require('fs')
-const zlib = require('zlib')
+const http = require('http')
+const httpServer = http.createServer((req, res) => {
+    if (req.url === '/')
+        res.write("Welcome Home!")
+    else
+        res.write('Hello World!')
+    res.end()
+})
 
-const readStream = fs.createReadStream("example.txt", "utf8")
-const writeStream = fs.createWriteStream("example1.txt")
-
-// sourceStream | destinationStream
-// readStream.pipe(writeStream)
-
-const gZip = zlib.createGzip()
-const gZipWriteStream = fs.createWriteStream("example1.txt.gz")
-readStream.pipe(gZip).pipe(gZipWriteStream)
-
-
-setTimeout(() => {
-    const gZipReadStream = fs.createReadStream("./example1.txt.gz")
-    const gUnzip = zlib.createGunzip()
-    gUnzip.on('error', error => console.log(error))
-    const gUnzipWriteStream = fs.createWriteStream("uncompressed.txt")
-    gZipReadStream.pipe(gUnzip).pipe(gUnzipWriteStream)
-}, 2000);
+httpServer.listen(3000)
