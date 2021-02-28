17e2d6d617ca04106437f25c4e3621383832051f
nikhiljainlive <nikhiljain5195@gmail.com>
2021-02-28 12:01:23 +0530

    Implemented installing dependencies through npm

--- app.js
+++ app.js
@@ -1,28 +1,4 @@
-const fs = require('fs')
-const http = require('http')
+ const loadash = require('lodash')
 
-http.createServer((req, res) => {
-    if (req.url == '/htmlContent') {
-        const readStream = fs.createReadStream('./static/index.html')
-        res.writeHead(200, { 'Content-Type': 'text/html' })
-        readStream.pipe(res)
-        return
-    }
-
-    if (req.url == '/imageContent') {
-        const readStream = fs.createReadStream('./static/example.jpg')
-        res.writeHead(200, { 'Content-Type': 'image/jpeg' })
-        readStream.pipe(res)
-        return
-    }
-
-    if (req.url == '/jsonContent') {
-        const readStream = fs.createReadStream('./static/example.json')
-        res.writeHead(200, { 'Content-Type': 'application/json' })
-        readStream.pipe(res)
-        return
-    }
-
-    res.write('Hello World')
-    res.end()
-}).listen(3000)
+ let example = loadash.fill([1, 2, 3, 4, 5], "banana", 1, 4)
+ console.log(example)
