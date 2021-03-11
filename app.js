a96c0f14e4d448ef573053af71173272d75c3f24
nikhiljainlive <nikhiljain5195@gmail.com>
2021-03-11 22:12:17 +0530

    Implemented params and query params in express

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
- const loadash = require('lodash')
+const express = require('express')
+const app = express()
 
- let example = loadash.fill([1, 2, 3, 4, 5], "banana", 1, 4)
- console.log(example)
+app.get('/', (req, res) => {
+    res.send("Hello World")
+})
+
+app.get('/example', (req, res) => {
+    res.send("hitting example route")
+})
+
+app.get('/example/:name/:age', (req, res) => {
+    console.log(req.params.name)
+    console.log(req.params.age)
+    console.log(req.query)
+    res.send("example with route params " + req.params.name + " " + req.params.age + "<br>Your query paramaters " + req.query.name)
+})
+
+app.listen(3000)
+
+
+// TODO :implement express with body parser
