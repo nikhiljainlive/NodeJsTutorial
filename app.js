08ac02ab193791e686f4c6df71832f461364f330
nikhiljainlive <nikhiljain5195@gmail.com>
2021-04-06 00:11:08 +0530

    Add. added complex user input validation with Joi validation library

--- app.js
+++ app.js
@@ -3,32 +3,41 @@ const path = require('path')
 const app = express()
 const bodyParser = require('body-parser')
 const joi = require('joi')
+const { preferences } = require('joi')
 
-// app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-app.use('/public', express.static(path.join(__dirname, 'static')))
+const arrayString = ['banana', 'mango', 'apple']
+const arrayOfObjects = [{ example: 'example1' }, { example: 'example2' }, { example: 'example3' }]
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'static', 'index.html'))
+const userInput = {
+    personalInfo: {
+        streetAddress: '123456789',
+        city: 'Bhopal',
+        state: 'MP'
+    },
+    preferences: arrayString,
+    examples: arrayOfObjects
+}
+
+const personalInfoSchema = joi.object().keys({
+    streetAddress: joi.string().required(),
+    city: joi.string().required(),
+    state: joi.string().length(2).required()
+})
+const preferencesSchema = joi.array().items(joi.string())
+
+const exampleSchema = joi.array().items(joi.object().keys({
+    example: joi.string().required()
+}))
+
+const schema = joi.object().keys({
+    personalInfo: personalInfoSchema,
+    preferences: preferencesSchema,
+    examples: exampleSchema
 })
 
-app.post('/', (req, res) => {
-    console.log(req.body)
-
-    const schema = joi.object().keys({
-        email: joi.string().trim().email().required(),
-        pwd: joi.string().min(5).max(10).required()
-    })
-
-    const { error, value } = schema.validate(req.body)
-    if (error) {
-        console.log(error)
-        res.send('an error has occured')
-        return
-    }
-
-    console.log(value)
-    res.send('successfully posted the data')
-})
-
-app.listen(3000)
+const { error, value } = schema.validate(userInput)
+if (error) {
+    console.log(error)
+    return
+}
+console.log(value)
