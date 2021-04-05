a3707c7fcdba84759d03f11f05833c8c36a78ad0
nikhiljainlive <nikhiljain5195@gmail.com>
2021-04-05 22:46:05 +0530

    Add. added post request with AJAX and bodyParser json

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 const bodyParser = require('body-parser')
 
 app.use(bodyParser.urlencoded({ extended: false }))
-
+app.use(bodyParser.json())
 app.use('/public', express.static(path.join(__dirname, 'static')))
 
 app.get('/', (req, res) => {
@@ -13,11 +13,11 @@ app.get('/', (req, res) => {
 
 app.post('/', (req, res) => {
     console.log(req.body)
-    console.log(req.body.email)
-    console.log(req.body.pwd)
+    console.log(req.body[0].value)
+    console.log(req.body[1].value)
 
     // TODO :: do something with email and password
-    res.send('successfully posted data')
+    res.json({ success: true })
 })
 
 app.listen(3000)
