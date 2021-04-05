1cd5ae258968b20dd46682802d9428fa33e5377a
nikhiljainlive <nikhiljain5195@gmail.com>
2021-04-05 23:11:41 +0530

    Add. Added simple input validation with bodyParser and Joi

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const bodyParser = require('body-parser')
+const joi = require('joi')
 
-app.use(bodyParser.urlencoded({ extended: false }))
+// app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use('/public', express.static(path.join(__dirname, 'static')))
 
@@ -13,11 +14,21 @@ app.get('/', (req, res) => {
 
 app.post('/', (req, res) => {
     console.log(req.body)
-    console.log(req.body[0].value)
-    console.log(req.body[1].value)
 
-    // TODO :: do something with email and password
-    res.json({ success: true })
+    const schema = joi.object().keys({
+        email: joi.string().trim().email().required(),
+        pwd: joi.string().min(5).max(10).required()
+    })
+
+    const { error, value } = schema.validate(req.body)
+    if (error) {
+        console.log(error)
+        res.send('an error has occured')
+        return
+    }
+
+    console.log(value)
+    res.send('successfully posted the data')
 })
 
 app.listen(3000)
