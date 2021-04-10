3640eb3e507c3e10a62beca63365ef712f0c93d0
nikhiljainlive <nikhiljain5195@gmail.com>
2021-04-11 00:15:04 +0530

    Add. Creating and rendering dynamic HTML pages with ejs

--- app.js
+++ app.js
@@ -2,42 +2,23 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const bodyParser = require('body-parser')
-const joi = require('joi')
-const { preferences } = require('joi')
 
-const arrayString = ['banana', 'mango', 'apple']
-const arrayOfObjects = [{ example: 'example1' }, { example: 'example2' }, { example: 'example3' }]
+app.use('/public', express.static(path.join(__dirname, 'static')))
+app.set('view engine', 'ejs')
 
-const userInput = {
-    personalInfo: {
-        streetAddress: '123456789',
-        city: 'Bhopal',
-        state: 'MP'
-    },
-    preferences: arrayString,
-    examples: arrayOfObjects
-}
+// app.use('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'static', 'index.html'))
+// })
 
-const personalInfoSchema = joi.object().keys({
-    streetAddress: joi.string().required(),
-    city: joi.string().required(),
-    state: joi.string().length(2).required()
-})
-const preferencesSchema = joi.array().items(joi.string())
-
-const exampleSchema = joi.array().items(joi.object().keys({
-    example: joi.string().required()
-}))
-
-const schema = joi.object().keys({
-    personalInfo: personalInfoSchema,
-    preferences: preferencesSchema,
-    examples: exampleSchema
+app.use('/:userQuery', (req, res) => {
+    res.render('index', {
+        data: {
+            userQuery: req.params.userQuery,
+            searchResults: ['book1', 'book2', 'book3'],
+            isLoggedIn: true,
+            username: "nikhiljainlive"
+        }
+    })
 })
 
-const { error, value } = schema.validate(userInput)
-if (error) {
-    console.log(error)
-    return
-}
-console.log(value)
+app.listen(3000)
